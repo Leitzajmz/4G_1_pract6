1dea83cd355f23c2d1a6b819a264aac92d143500
LeitzaJmz <yoshia_98@hotmail.com>
2018-06-02 17:42:04 -0500

    mostrar multiplexado en el display (Aun no funciona)

--- main.c
+++ main.c
@@ -3,20 +3,48 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
-#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
+int contadorD, numeroD;
+long contadorRetardo;
+int display[10] = {63,6,91,79,102,109,125,7,127,103};
 
-#ifdef __DEBUG_SERIAL__
-   #define TX_232        PIN_C6
-   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1)
-   #use fast_io(c)
-#endif
-
+void mostrar();
+#int_timer0
+void timer0(){
+   contadorD++;
+}
 void main (void){
    setup_oscillator(OSC_16MHZ);
-#ifdef __DEBUG_SERIAL__ //Deberiamos de proteger nuestras depuraciones de esta forma o usar una macro ya protegida.
-   printf("Hola Mundo\n");//Puedes usar putc o printf. Revisa la documentación de CCS para ver que mas puedes hacer.
-#endif
+   setup_timer_0(RTCC_INTERNAL |RTCC_DIV_128 | T0_8_BIT);
+   enable_interrupts(int_timer0);
+   enable_interrupts(GLOBAL);
+   
+   set_tris_d(0x00); //PORTD salida
+   set_tris_e(0x00);//PORTE salida
+   set_tris_a(0x00);
+   int indiceD = 0x00;
+   numeroD = 0;
    while(1){
-      
+      numeroD = 0;
+      while (numeroD < 100){
+         numeroD++;
+         if(contadorD > 122 ){
+            if(indiceD > 9)
+               indiceD = 0x00;
+            mostrar();
+            contadorD = 0x00;
+         }
+      }
    }
-}	
+}
+
+void mostrar(){
+   int unidades = 0x00, decenas = 0x00;
+   decenas = numeroD / 10;
+   unidades = numeroD % 10;
+   output_d(display[5]);
+   output_a(0x01);
+   delay_ms(1);
+   output_d(display[4]);
+   output_a(0x02);
+   delay_ms(1);
+}

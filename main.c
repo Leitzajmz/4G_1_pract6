dd75283299bf3e69fb2452c33e47cadec85782c8
LeitzaJmz <yoshia_98@hotmail.com>
2018-06-02 17:59:12 -0500

    Funcion del bus de control, contador de anillo con un delay de 10ms lo que hace que cada uno de los leds prenda 100 veces por segundo

--- main.c
+++ main.c
@@ -2,49 +2,25 @@
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
+#use fast_io(a)
+   int controlDisplay = 0x01;
+void busDeControl();
 
-int contadorD, numeroD;
-long contadorRetardo;
-int display[10] = {63,6,91,79,102,109,125,7,127,103};
-
-void mostrar();
-#int_timer0
-void timer0(){
-   contadorD++;
-}
 void main (void){
    setup_oscillator(OSC_16MHZ);
-   setup_timer_0(RTCC_INTERNAL |RTCC_DIV_128 | T0_8_BIT);
-   enable_interrupts(int_timer0);
-   enable_interrupts(GLOBAL);
-   
-   set_tris_d(0x00); //PORTD salida
-   set_tris_e(0x00);//PORTE salida
-   set_tris_a(0x00);
-   int indiceD = 0x00;
-   numeroD = 0;
+   set_tris_a(0x00); //PORTA salida
    while(1){
-      numeroD = 0;
-      while (numeroD < 100){
-         numeroD++;
-         if(contadorD > 122 ){
-            if(indiceD > 9)
-               indiceD = 0x00;
-            mostrar();
-            contadorD = 0x00;
-         }
-      }
+      busDeControl();
    }
 }
 
-void mostrar(){
-   int unidades = 0x00, decenas = 0x00;
-   decenas = numeroD / 10;
-   unidades = numeroD % 10;
-   output_d(display[5]);
-   output_a(0x01);
-   delay_ms(1);
-   output_d(display[4]);
-   output_a(0x02);
-   delay_ms(1);
+void busDeControl(){
+
+   output_a(controlDisplay);
+   controlDisplay <<= 0x01;
+   delay_ms(50);
+   if(controlDisplay == 0x10)
+      controlDisplay = 0x01;
 }
+
+

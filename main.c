3c8d1246b513bbfe3a07eab9ee8002a2b6d4c245
LeitzaJmz <yoshia_98@hotmail.com>
2018-06-02 22:25:39 -0500

    Cambio de estado por medio de la interrupcion externa

--- main.c
+++ main.c
@@ -3,14 +3,34 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 #use fast_io(a)
-   int controlDisplay = 0x01;
+#use fast_io(b)
+#use fast_io(e)
+int controlDisplay = 0x01;
+int variableSensada = 0x00;
 void busDeControl();
 
+#int_ext
+void ISR_RB(){
+   variableSensada++;
+}
 void main (void){
    setup_oscillator(OSC_16MHZ);
    set_tris_a(0x00); //PORTA salida
+   set_tris_b(0x01); // PIN_B4 entrada
+   set_tris_e(0x00);
+   enable_interrupts(INT_EXT);
+   enable_interrupts(GLOBAL);
+   ext_int_edge(L_TO_H); 
+   
    while(1){
-      busDeControl();
+      if (variableSensada == 0x01){
+         output_e(0x01);
+      }
+      else if (variableSensada == 0x02){
+         output_e(0x02);
+         variableSensada = 0x00;
+      }
+      //busDeControl();
    }
 }
 

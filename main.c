79e1e3ede99261e6e9d008b019a09e83f3b884a5
LeitzaJmz <yoshia_98@hotmail.com>
2018-06-03 16:00:33 -0500

    Despliegue de datos en 4 displays multiplexados

--- main.c
+++ main.c
@@ -6,34 +6,48 @@
 #use fast_io(d)
 
 int controlGndDisplay = 0x01;
-int numero = 51;
+int numero = 84;
+int display[10] = {63,6,91,79,102,109,125,7,127,103};
 
 void busDeControl();
+void mostrarDatosDislpay(int numero);
 
 void main (void){
    setup_oscillator(OSC_16MHZ);
    set_tris_a(0x00); //PORTA puerto de salida
    set_tris_d(0x00); //PORTD puerto de salida
    int decenas, unidades;
-   int display[10] = {63,6,91,79,102,109,125,7,127,103};
+   
    while(1){
       decenas = numero / 10;
       unidades = numero % 10;
-      output_a(0x08);
-      output_d(display[5]);
-      delay_ms(10);
-      output_a(0x04);
-      output_d(display[9]);
-      delay_ms(10);
+      
+      mostrarDatosDislpay(decenas);
+      busDeControl();
+      mostrarDatosDislpay(unidades);
+      busDeControl();
+      mostrarDatosDislpay(0x00);
+      busDeControl();
+      mostrarDatosDislpay(0x00);
+      busDeControl();
    }
 }
 
-/*void busDeControl(){
+void mostrarDatosDislpay(int numero){
    output_a(controlGndDisplay);
+   if (controlGndDisplay == 0x02)
+      output_d(display[numero]+128);
+   else
+      output_d(display[numero]);
+}
+
+void busDeControl(){
+   delay_ms(14);   
    controlGndDisplay <<= 0x01;
-   //delay_ms(10);
    if(controlGndDisplay == 0x10)
       controlGndDisplay = 0x01;
-}*/
+}
+
+
 
 

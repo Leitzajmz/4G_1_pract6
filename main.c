aeca01b0f1229599857314004e809625c6e535ba
LeitzaJmz <yoshia_98@hotmail.com>
2018-06-05 21:40:15 -0500

    funciones del sensor DHT11 agregadas.

--- main.c
+++ main.c
@@ -1,71 +1,141 @@
 #include <18F4620.h>
-#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
-#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
-#use delay(clock=16000000)
+#include <stdio.h>
+#include <stdlib.h>
 #use fast_io(a)
 #use fast_io(d)
 #use fast_io(b)
 #use fast_io(e)
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
+#use delay(clock=16000000)
 
-int controlGndDisplay = 0x01;
-int numero = 84;
+#BIT dataDht = 0xF82.4             //pin de dato de entrada del DHT11 portc
+#BIT dht_io = 0xF94.4  //bit 0 del tris b para ponerlo como entrada o salida
+int temperatura, temperaturaDecimal, humidity, humidityDecimal, checksum ;
+short timeOut;
+int cambio=0x01;
+int contador=0x01;
 int display[10] = {63,6,91,79,102,109,125,7,127,103};
-int variableSensada = 0x00;
+unsigned int dato[9] ={0} ;
 
-void busDeControl();
-void mostrarDatosDislpay(int numero);
+void inicioDht11();
+short respuesta();
+unsigned int readData();
+void mostrarDatos();
+
+#INT_TIMER0
+void timer0(){
+   contador <<= 1;
+   if(contador>=16){
+      contador=1;
+   } 
+   set_timer0(120);
+   mostrarDatos();
+}
 
 #int_ext
-void ISR_RB(){
-   variableSensada++;
+void ISR_RB0(){
+   if (cambio == 2)
+      cambio = 0 ;
+   cambio++;
 }
-void main (void){
+
+void main(void) {
    setup_oscillator(OSC_16MHZ);
-   set_tris_a(0x00); //PORTA puerto de salida
-   set_tris_d(0x00); //PORTD puerto de salida
-   set_tris_b(0x01); // PIN_B4 entrada
-   set_tris_e(0x00);
-   
+   set_tris_d(0x00);
+   set_tris_a(0x00);
+   set_tris_e(0x00); 
+   set_tris_b(0xFF); 
+   ext_int_edge(L_TO_H);
+   setup_timer_0(RTCC_INTERNAL | RTCC_DIV_128 | RTCC_8_BIT);
+   enable_interrupts(INT_TIMER0);
    enable_interrupts(INT_EXT);
    enable_interrupts(GLOBAL);
-   ext_int_edge(L_TO_H); 
-  
-   int decenas, unidades;
+   set_timer0(120);
    
-   while(1){
-      if (variableSensada == 0x01){
-         output_e(0x01);
+    while (1) { 
+      //output_e(cambio);
+      inicioDht11();
+      if(respuesta()){                    
+         humidity = readData();          
+         humidityDecimal = readData();                 
+         temperatura = readData();                   
+         temperaturaDecimal = readData();                     
+         checksum = readData();                  
+         if(checksum==(temperatura+temperaturaDecimal+humidity+humidityDecimal)){
+            switch(cambio){
+               case 1:
+                  output_e(cambio);
+                  dato[1]=(int)temperatura/10;
+                  dato[2]=(int)temperatura%10;
+                  dato[4]=(int)temperaturaDecimal/10;
+                  dato[8]=(int)temperaturaDecimal%10;
+               break;
+               case 2:
+                  output_e(cambio);
+                  dato[1]=(int)humidity/10;
+                  dato[2]=(int)humidity%10;
+                  dato[4]=(int)humidityDecimal/10;
+                  dato[8]=(int)humidityDecimal%10;
+               break;
+            }
+         }
       }
-      else if (variableSensada == 0x02){
-         output_e(0x02);
-         variableSensada = 0x00;
-      }
-     
-      decenas = numero / 10;
-      unidades = numero % 10;
-      
-      mostrarDatosDislpay(decenas);
-      busDeControl();
-      mostrarDatosDislpay(unidades);
-      busDeControl();
-      mostrarDatosDislpay(0x00);
-      busDeControl();
-      mostrarDatosDislpay(0x00);
-      busDeControl();
    }
 }
-
-void mostrarDatosDislpay(int numero){
-   output_a(controlGndDisplay);
-   if (controlGndDisplay == 0x02)
-      output_d(display[numero]+128);
-   else
-      output_d(display[numero]);
+void mostrarDatos(){
+   if(contador==2){
+         output_a(contador);
+         output_d(display[dato[contador]]+128);
+      }
+      else{
+         output_a(contador);
+         output_d(display[dato[contador]]);
+      } 
+}
+void inicioDht11(){
+   dht_io = 0;  //configuracion del pin C4 como salida
+   dataDht = 0;       //se encia un 0 al sensor
+   delay_ms(18);
+   dataDht = 1;
+   delay_us(30);
+   dht_io = 1;  //configuracion de pin C4 como entrada
 }
 
-void busDeControl(){
-   delay_ms(14);   
-   controlGndDisplay <<= 0x01;
-   if(controlGndDisplay == 0x10)
-      controlGndDisplay = 0x01;
+short respuesta(){
+   delay_us(40);
+   if(!dataDht){                     // Read and test if connection pin is low
+      delay_us(80);
+      if(dataDht){                    // Read and test if connection pin is high
+         delay_us(50);
+         return 1;}
+   }
 }
+unsigned int readData(){
+   unsigned int i, k, data = 0;     // k is used to count 1 bit reading duration
+   if(timeOut)
+      break;
+   for(i = 0; i < 8; i++){
+      k = 0;
+      while(!dataDht){                          // Wait until pin goes high
+         k++;
+         if (k > 100) {
+            timeOut = 1; 
+            break;
+         }
+         delay_us(1);
+      }
+      delay_us(30);
+      if(!dataDht)
+         bit_clear(data, (7 - i));               // Clear bit (7 - i)
+      else{
+         bit_set(data, (7 - i));                 // Set bit (7 - i)
+         while(dataDht){                         // Wait until pin goes low
+            k++;
+            if (k > 100) {timeOut = 1; break;}
+               delay_us(1);}
+      }
+   }
+   return data;
+}
+   
